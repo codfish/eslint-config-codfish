ae1881252e2c4495d5aa546ef3ac95aa2ea09445
Chris O'Donnell <dev@codfish.io>
2019-01-22 17:09:12 -0500

    feat: leverage prettier config and plugin
    BREAKING CHANGE, implement #6

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const { ifAnyDep } = require('./utils');
 
 module.exports = {
-  extends: [ifAnyDep('react', 'airbnb', 'airbnb-base'), './jest.js'],
+  extends: [
+    // Airbnb config as the foundation
+    ifAnyDep('react', 'airbnb', 'airbnb-base'),
+
+    // Kent C. Dodds' Jest config
+    ifAnyDep('jest', 'kentcdodds/jest'),
+
+    // Prettier plugin includes config, plugin & rules declaration
+    'plugin:prettier/recommended',
+
+    // Prettier react rules
+    ifAnyDep('react', 'prettier/react'),
+  ].filter(Boolean),
 
   rules: {
-    // rules which airbnb & prettier will conflict to the point which prevents
-    // linting from passing. They're avoidable but it can be quite annoying.
-    'function-paren-newline': ['error', 'consistent'],
-    // prettier wants to break up long lines and won't wrap in parens, airbnb's
-    // eslint rules enforce location of function bodies.
-    'implicit-arrow-linebreak': 'off',
-
-    // the one opinionated rule override...
-    // https://github.com/airbnb/javascript/pull/985#issuecomment-239145468
-    // airbnb says yes but react team says no...I choose to defer to react team
+    // One opinionated rule override...
+    // Airbnb says yes but react team says no...I choose to defer to react team
     // in this scenario.
+    // @see https://github.com/airbnb/javascript/pull/985#issuecomment-239145468
     'react/jsx-filename-extension': 'off',
   },
 
@@ -28,7 +33,7 @@ module.exports = {
   // sane overrides for test files
   overrides: [
     {
-      files: ['**/__tests__/**/*.js', '**/test/**/*.js', '*test.js', '*.spec.js'],
+      files: ['**/__tests__/**/*.js', '**/test/**/*.js', '*.test.js', '*.spec.js'],
       env: {
         mocha: true,
         jest: true,
