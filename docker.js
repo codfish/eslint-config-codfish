87039ed2b5a446cd06b0c732e278235b1321a45c
Chris O'Donnell <dev@codfish.io>
2019-09-17 23:40:36 -0400

    feat: include no-unresolved rule for docker purposes
    BREAKING CHANGE: Removes docker.js extention and instead automatically
    applies those rules (only one for import/no-unresolved) based on
    whether theres a Dockerfile or docker-compose.yml file in the root

--- docker.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    // should use this if you're using docker. node_modules should be installed
-    // and present in your containers, but not needed/mounted on the host
-    // machine. eslint will complain that it can't find node_modules imports.
-    // this will ignore the rule for imports that don't start with a period.
-    'import/no-unresolved': ['error', { ignore: ['^[^.]+'] }],
-  },
-};
--- utils.js
+++ utils.js
@@ -6,13 +6,19 @@
  * @see https://github.com/kentcdodds/kcd-scripts/blob/master/src/utils.js
  */
 const fs = require('fs');
+const path = require('path');
 const readPkgUp = require('read-pkg-up');
 const arrify = require('arrify');
 const has = require('lodash.has');
 
-const { pkg } = readPkgUp.sync({
+const { package: pkg, path: pkgPath } = readPkgUp.sync({
   cwd: fs.realpathSync(process.cwd()),
 });
+const appDirectory = path.dirname(pkgPath);
+
+const fromRoot = (...p) => path.join(appDirectory, ...p);
+const hasFile = (...p) => fs.existsSync(fromRoot(...p));
+const ifFile = (files, t, f) => (arrify(files).some(file => hasFile(file)) ? t : f);
 
 const hasPkgProp = props => arrify(props).some(prop => has(pkg, prop));
 const hasPkgSubProp = pkgProp => props => hasPkgProp(arrify(props).map(p => `${pkgProp}.${p}`));
@@ -23,4 +29,4 @@ const hasPeerDep = hasPkgSubProp('peerDependencies');
 const hasAnyDep = args => [hasDep, hasDevDep, hasPeerDep].some(fn => fn(args));
 const ifAnyDep = (deps, t, f) => (hasAnyDep(arrify(deps)) ? t : f);
 
-module.exports = { ifAnyDep };
+module.exports = { ifAnyDep, ifFile };
