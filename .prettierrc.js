8417056fc7b6a6f763b79741a6328fe3ded02c23
Chris O'Donnell <dev@codfish.io>
2019-09-17 22:31:42 -0400

    chore: cleanup linting configuration

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,11 @@
 module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
   singleQuote: true,
   trailingComma: 'all',
-  printWidth: 100,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  proseWrap: 'always',
 };
